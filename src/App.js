2eb2f538387e2abb627f83c749297e949c573800
Amesh Alex Kuruvilla <finixbenny@Ameshs-MacBook-Air.local>
2020-09-30 14:30:31 -0500

    made navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar/Navbar.js'
-import {BrowserRouter as Router} from 'react-router-dom';
+import NavBar from './components/Navbar.js'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Projects from './pages/Projects.js'
+import Home from './pages/Home.js'
+import About from './pages/About.js'
+import Minesweeper from './pages/Minesweeper.js'
 
 function App() {
   return (
     <Router>
-      <NavBar></NavBar>
+      <NavBar/>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/projects' exact component={Projects} />
+        <Route path='/minesweeper' exact component={Minesweeper} />
+        <Route path='/about' exact component={About} />
+        
       
+      </Switch> 
     </Router>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+// import './Button.css'
+// import { Link } from 'react-router-dom'
+
+// export function Button() {
+//     return (
+//         <Link to=''
+//     )
+// }
