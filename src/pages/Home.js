e5e93f152ad97061e12e19bf8365b48c8282c0ad
Amesh Alex Kuruvilla <finixbenny@Ameshs-MacBook-Air.local>
2020-10-01 12:22:36 -0500

    added resume content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,79 @@ export default function Home () {
     return (
         <>
         
-        <h1 className="projects"> Home</h1>
+        <div className="mainpage"> 
+            <h1>Amesh Alex Kuruvilla</h1>
+            <br></br>
+            
+            <h4> 
+                <ul className="skillscontainer">
+                    <img className="skillsitem" src="/images/html-5.svg" alt="triangle with all three sides equal" height="87" width="100" />
+                    {/* <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+
+                    <img className="skillsitem" src="/images/css.svg" alt="triangle with all three sides equal" height="87" width="100" />
+                    {/* <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+
+                    <img className="skillsitem" src="/images/javascript.svg" alt="triangle with all three sides equal" height="87" width="100" />
+                    {/* <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+
+                    <img className="skillsitem" src="/images/React-icon.svg" alt="triangle with all three sides equal" height="87" width="100" />
+                    {/* WIKIPEDIA */}
+
+                    <img className="skillsitem" src="/images/nodejs.svg" alt="triangle with all three sides equal" height="87" width="100" />
+                    {/* <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+
+                    <img className="skillsitem" src="/images/expressjs.svg" alt="triangle with all three sides equal" height="87" width="100" />
+                    {/* https://www.vectorlogo.zone/logos/expressjs/index.html */}
+
+                    <img className="skillsitem" src="/images/mongodb.svg" alt="triangle with all three sides equal" height="87" width="100" />
+                    {/* https://worldvectorlogo.com/downloaded/mongodb */}
+
+                    <img className="skillsitem" src="/images/ruby.svg" alt="triangle with all three sides equal" height="87" width="100" />
+                    {/* <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+
+                    <img className="skillsitem" src="/images/database.svg" alt="triangle with all three sides equal" height="87" width="100" />
+                    {/* <div>Icons made by <a href="https://www.flaticon.com/authors/phatplus" title="phatplus">phatplus</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+                </ul>
+            </h4>
+            <div>
+                <h4>EXPERIENCE</h4>
+                <h5>General Assembly -  Software Engineering Fellowship</h5>
+                <h5>JUL 2020 - OCT 2020</h5>
+                <br></br>
+                <br></br>
+                <div>
+                    <h6>
+                        Fellowship for 12 weeks which included designing, developing, testing, debugging and troubleshooting Full Stack Apps / projects in an agile environment. 
+                    </h6>
+                    <br></br>
+                    <h6>
+                        Created Minesweeper Game using JQUERY.
+                    </h6>
+                    <br></br>
+                    <h6>
+                        Created a cryptocurrency trading app with React.
+                    </h6>
+                    <br></br>
+                </div>
+                <br></br><br></br><br></br>
+                <h5>SITA, India — Team Leader</h5>
+                <h5>AUG 2016 - AUG 2019</h5>
+                <br></br>
+                <br></br>
+                <div>
+                    <h6>
+                    Testing In-house custom software for Business Operations and improved code logic and User Interface. 
+                    </h6>
+                    <br></br>
+                    <h6>
+                    Strengthened relationships with 5  strategic Travel Websites based in Germany with Product and Content management.
+                    </h6>
+                    <br></br>
+                    
+                </div>
+            </div>
+
+        </div>
         
         
         </>
